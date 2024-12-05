ff8bea0a97fe9a5cd095572100f901e3d765a0f6
Piyush Anand <piyushanand2580@gmail.com>
2024-12-05 16:56:32 +0530

    init

---
+++ src/main/java/com/inventorymanagementsystem/server/InventorymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventorymanagementsystem.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorymanagementsystemApplication.class, args);
+	}
+
+}
